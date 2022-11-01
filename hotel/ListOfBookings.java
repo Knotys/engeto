11ed723d1554f301823769a27d959383b4b1ddc6
Robert Knotek <109803119+Knotys@users.noreply.github.com>
2022-11-01 23:08:33 +0100

    Add files via upload

---
+++ hotel/ListOfBookings.java
@@ -0,0 +1,27 @@
+package com.engeto.hotel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfBookings {
+    List<Booking> listOfBookings = new ArrayList<>();
+
+
+    public void addBooking(Booking newBooking) {
+        listOfBookings.add(newBooking);
+    }
+
+    public int numberOfBookings() {
+        return listOfBookings.size();
+    }
+
+    public void printAllBookings() {
+        for (Booking booking : listOfBookings)
+        {
+            System.out.println("    "+"Rezervace na jméno: "+booking.guest.getDescription()+
+                    "; Pokoj: "+booking.room.getDescription()+"; Datum příjezdu: "+booking.getDateOfArrival()+
+                    "; Datum odjezdu: "+booking.getDateOfDeparture()+
+                    "; "+booking.typeOfVacation+" pobyt");
+        }
+    }
+}
---
+++ hotel/TypeOfVacation.java
@@ -0,0 +1,5 @@
+package com.engeto.hotel;
+
+public enum TypeOfVacation {
+    Pracovní, Rekreační
+}
